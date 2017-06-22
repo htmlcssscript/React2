40c86a836dd2acee243588a810b1e67362eedecf
htmlcssscript <3239089064@qq.com>
2017-06-22 11:16:29 +0800

    start

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React, {Component} from 'react';
+import TodoInput from './TodoInput';
+import TodoItem from './TodoItem';
+import 'normalize.css';
+import './reset.css';
+import './App.css';
+import './index.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            newTodo: '',//用户输入的文字
+            todoList: []
+        }
+    }
+
+    render() {
+        let todos = this.state.todoList//!true->过滤没有选中的数据
+                   .filter(item => !item.deleted) 
+                   .map((item,index) => {
+                     return (
+                      <li key={index}>
+                          <TodoItem todo={item} 
+                          onToggle={this.toggle.bind(this)}
+                          onDelete={this.delete.bind(this)}
+                          />
+                      </li>
+                   )
+                })
+        console.log(todos)
+        return (
+            <div className="App">
+                <h1>我的代办</h1>
+                <div className="inputWrapper">
+                    {/*<input type="text" value={this.state.newTodo} />*/}
+                    <TodoInput
+                      content={this.state.newTodo}
+                      onSubmit={this.addTodo.bind(this)}
+                      onChange={this.changeTitle.bind(this)}
+                     />
+                </div>
+                <ol className="todoList">
+                    {todos}
+                </ol>
+            </div>
+        )
+    }
+    addTodo(e) {
+       this.state.todoList.push({
+           id: idMaker(),
+           title: e.target.value,
+           status: null,
+           deleted: false
+       })
+       this.setState({
+           newTodo: '',
+           todoList: this.state.todoList
+       })
+    }
+    changeTitle(e) {
+        this.setState({
+            newTodo: e.target.value,
+            todoList: this.state.todoList
+        })
+    }
+    toggle(e,todo){
+      todo.status = todo.status === 'completed'?'': 'completed'
+      this.setState(this.state)
+    }
+    delete(e,todo){
+      todo.deleted = true
+      this.setState(this.state)
+    }
+}
+
+export default App
+
+let id = 0
+
+function idMaker() {
+  id += 1
+  return id
+}
---
+++ src/leanCloud.js
@@ -0,0 +1,10 @@
+import AV from 'leancloud-storage'
+var APP_ID = '8DLfPy2gPqt79vgvx84vAYv7-gzGzoHsz';
+var APP_KEY = '3U3rB4iA3GwjwPQzK7NnccJn';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
+export default AV
